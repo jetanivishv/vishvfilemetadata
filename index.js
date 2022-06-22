627e40494933e325e24512bf9ce9eca0ba3aa7d8
vishv <jetanivishv@gmail.com>
2022-06-22 17:37:03 +0530

    hi

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer=require('multer');
+var bodyParser= require('body-Parser');
+var upload = multer({ dest: './public' })
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.route('/api/fileanalyse')
+.post(upload.single('upfile'),(req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  })
+});
 
 
 const port = process.env.PORT || 3000;
